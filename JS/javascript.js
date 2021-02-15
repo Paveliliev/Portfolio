30fc7f851577f2d4f0ac2d1127af704a87bf9f98
Paveliliev <pavelnili94@gmail.com>
2021-02-15 16:59:14 +0000

    Add project files.

---
+++ JS/javascript.js
@@ -0,0 +1,7 @@
+
+//document.getElemtById("intro").style.backgroundColor = "red";
+//document.getElementById("customer1").setAttribute("style", "font-weight:bold");
+//var e = document.getElementById("intro");
+
+document.getElementById("mainTitle").addEventListener("click", function(){alert("Hello again");});
+
